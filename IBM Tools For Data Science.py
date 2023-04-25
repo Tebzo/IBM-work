66b35cc0364fb4a551c0bad58273bfe04421c735
Mutebi Henry <97189331+Tebzo@users.noreply.github.com>
2023-04-25 06:18:25 -0700

    Add files via upload

---
+++ IBM Tools For Data Science.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Clean Energy
+
+# ## Introduction to Clean Energy
+# Clean Energy is one of the reasons Data science can help to bring impact to the community
+
+# ### Here's a markdown cell listing some popular data science languages:
+# 
+# - Python
+# - R
+# - SQL
+# 
+# Python is a popular general-purpose programming language that has gained significant traction in the data science community due to its ease of use, flexibility, and large library of data science tools and packages such as Pandas, NumPy, and Scikit-learn.
+# 
+# R is another popular language used for statistical computing and graphics. It is particularly well-suited for data exploration, visualization, and statistical analysis due to its vast collection of statistical and graphical packages.
+# 
+# SQL (Structured Query Language) is a standard programming language used to manage and manipulate relational databases. It is an essential tool for working with large datasets, querying data, and performing data cleaning and transformation tasks.
+
+# # Data Science Libraries
+# 
+# Here are some popular data science libraries that can be used in Python:
+# 
+# - **NumPy:** A library for numerical computing in Python, providing support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions.
+# - **Pandas:** A library for data manipulation and analysis. It provides data structures for efficiently storing and manipulating large datasets, as well as tools for reading and writing data to various file formats.
+# - **Scikit-learn:** A library for machine learning in Python, providing tools for data mining, data analysis, and data visualization. It includes a variety of classification, regression, clustering, and dimensionality reduction algorithms.
+# - **Matplotlib:** A library for creating visualizations in Python. It provides tools for creating a wide range of plots and charts, from simple line graphs to complex 3D visualizations.
+# - **Seaborn:** A library for creating statistical graphics in Python. It provides a high-level interface for creating informative and attractive visualizations of statistical data.
+# - **TensorFlow:** A library for building and training machine learning models. It provides a flexible and efficient platform for implementing a wide range of deep learning algorithms.
+# - **Keras:** A high-level neural networks API, written in Python and capable of running on top of TensorFlow, CNTK, or Theano. It allows for easy and fast prototyping of deep learning models.
+# 
+# These are just a few examples of the many powerful data science libraries available in Python.
+
+# ## Data Science Tools
+# 
+# | Tool        | Description                                                  |
+# |-------------|--------------------------------------------------------------|
+# | Python      | High-level programming language used for data analysis        |
+# | R           | Statistical programming language used for data analysis       |
+# | SQL         | Used for managing and manipulating data in relational databases|
+# | Tableau     | Data visualization tool                                       |
+# | Power BI    | Business intelligence tool                                    |
+# | Excel       | Spreadsheet software used for data analysis                    |
+# | Jupyter     | Interactive computing environment for data science workflows  |
+# | TensorFlow  | Open-source machine learning framework                         |
+# | PyTorch     | Open-source machine learning framework                         |
+# | Scikit-learn| Machine learning library for Python                            |
+# | Pandas      | Data manipulation and analysis library for Python              |
+# | Numpy       | Library for working with arrays and numerical operations in Python|
+
+# # Arithmetic Expression Examples
+# 
+# Arithmetic expressions are mathematical calculations that involve basic operations such as addition, subtraction, multiplication, and division. Here are some examples of arithmetic expressions:
+# 
+# 1. Addition: `2 + 3 = 5`
+# 2. Subtraction: `5 - 2 = 3`
+# 3. Multiplication: `4 * 5 = 20`
+# 4. Division: `10 / 2 = 5`
+# 
+# In addition to these basic operations, arithmetic expressions can also include parentheses, exponents, and modulus. For example:
+# 
+# 1. Parentheses: `(4 + 3) * 2 = 14`
+# 2. Exponents: `2 ** 3 = 8` (2 raised to the power of 3)
+# 3. Modulus: `7 % 3 = 1` (remainder of 7 divided by 3)
+# 
+# Arithmetic expressions are an important part of mathematics and programming, and are used extensively in various applications such as finance, engineering, and computer science.
+
+# In[1]:
+
+
+# Assigning values to variables
+num1 = 5
+num2 = 10
+num3 = 2
+
+# Multiplying num1 and num2
+product = num1 * num2
+
+# Adding num3 to the product
+result = product + num3
+
+# Printing the result
+print(result)
+
+
+# In[2]:
+
+
+minutes = 120 # Replace 120 with the number of minutes you want to convert
+
+hours = minutes / 60
+
+print(f"{minutes} minutes is equal to {hours} hours")
+
+
+# # Objectives
+# 
+# This notebook aims to achieve the following objectives:
+# 
+# 1. Provide an overview of Jupyter Notebook and its features.
+# 2. Introduce Markdown language and its syntax for creating rich-text documents.
+# 3. Demonstrate how to write and execute Python code in Jupyter Notebook.
+# 4. Show how to use Jupyter Notebook for data analysis and visualization.
+# 5. Share tips and tricks for optimizing the use of Jupyter Notebook.
+# 6. Encourage the use of Jupyter Notebook as a tool for scientific research and education.
+# 7. Answer common questions and troubleshoot issues related to Jupyter Notebook.
+
+# # Mutebi Henry
+
+# In[ ]:
+
+
+
+
